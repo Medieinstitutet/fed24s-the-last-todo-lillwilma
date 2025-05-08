1dbc41b0b608af82f13461dcb264fd556e69a672
lillwilma <[200~wilmasxs>
2025-05-08 13:51:04 +0200

    Fixade todo-listan!

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [todos, setTodos] = useState([  
+    {id: 1, title: "Städa ditt  rum!", done: false},
+    {id: 2, title: "Gå ut på solskensprommis!", done: false},
+    {id: 3, title: "Öva gitarr!", done: false},
+  ]);
+
+  const toggleDone = (id: number) => {
+    setTodos(todos.map((todo) => todo.id === id ? {...todo, done: !todo.done} : todo));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Min Todo-lista</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id} onClick={() => toggleDone(todo.id)}
+          style={{
+            textDecoration: todo.done ? "line-through" : "none",
+            cursor: "pointer",
+            color: todo.done ? "black" : "white",
+          }}>
+            {todo.title}    
+            </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default App
+
+export default App;
